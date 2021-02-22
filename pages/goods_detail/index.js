16f582e3c1734595d96da61d7df88da674e14e87
wangcheng <345301995@qq.com>
2021-02-22 19:16:23 +0800

    update

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,20 +1,60 @@
-// pages/goods_detail/index.js
+//引入 用来 发送请求的方法  一定要把路径补全
+import { request } from "../../request/index.js"
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+     goodsObj:{}
   },
-
+   GoodsInfo:{},
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const {goods_id}=options;
+    console.log(goods_id);
+    this.getGoodsDetail(goods_id)
   },
+async getGoodsDetail(goods_id){
+  const res=await request({url:'https://api-hmugo-web.itheima.net/api/public/v1/goods/detail',data:{goods_id}})
+  console.log(res.data.message);
+  this.GoodsInfo=res.data.message
+ this.setData({
+  goodsObj:res.data.message
+ })
+},
+//点击轮播图放大预览
+handlePrevewImage(e){
+  //构造遍历要预览的图片数组
+  const urls=this.GoodsInfo.pics.map(v=>v.pics_mid)
+  const current=e.currentTarget.dataset.url
+  wx.previewImage({
+    current,
+    urls,
+  })
+},
+handlleCartAdd(){
+  // console.log('ggggggggg');
+  let cart=wx.getStorageSync("cart")||[];
+  let index=cart.findIndex(v=>v.goods_id===this.GoodsInfo.goods_id);
+  if(index===-1){
+    this.GoodsInfo.num=1;
+    cart.push(this.GoodsInfo);
 
+  }else{
+    cart[index].num++;
+  }
+  wx.setStorageSync('cart',cart);
+  wx.showToast({
+    title: '加入成功',
+    mask:true,
+    icon:'success',
+
+  })
+},
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,20 +1,66 @@
-// pages/goods_list/index.js
+//引入 用来 发送请求的方法  一定要把路径补全
+import { request } from "../../request/index.js"
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+     tabs:[
+       {
+         id:0,
+         value:'综合',
+         isActive:true
+       },
+       {
+         id:1,
+         value:'销量',
+         isActive:false
+       },
+       {
+        id:2,
+        value:'价格',
+        isActive:false
+      }
+     ],
+     goodsList:[]
   },
-
+  //接口调用参数
+  QureeParams:{
+   query:'',
+   cid:'',
+   pagenum:1,
+   pagesize:10
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+      this.QureeParams.cid=options.cid;
+      this.getGoodsList()
+  },
+  // 获取商品列表的方法
+  async getGoodsList(){
+    const res=await request({url:'https://api-hmugo-web.itheima.net/api/public/v1/goods/search',data:this.QueryParams})
+    console.log(res.data.message.goods);
+    this.setData({
+      goodsList:res.data.message.goods
+    })
   },
 
+  //  标题点击事件
+  handleTabsItemChange(e){
+  //  console.log(e);
+  // 获取被点击的索引
+  const {index}=e.detail;
+  // 修改原数组
+  let {tabs}=this.data;
+  tabs.forEach((v,i)=>i===index?v.isActive=true:v.isActive=false);
+  //赋值到data中
+  this.setData({
+    tabs
+  })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -40,7 +86,7 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+      
   },
 
   /**
